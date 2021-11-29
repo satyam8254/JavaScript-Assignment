0077f9e5890527eafa3e720eb95f2fb3a3adede4
satyam8254 <satyamraj8254@gmail.com>
2021-11-29 12:25:19 +0530

    optimized code

--- assignments/can_drive/can_drive.js
+++ assignments/can_drive/can_drive.js
@@ -1,15 +1,11 @@
 function CanDrive(hasDrivingLiscence, isTired, isSober) {
     // Write you code here
-    if (hasDrivingLiscence == false && isTired == true && isSober == true) {
-        return "You cannot drive"
-    } else if (hasDrivingLiscence == false && isTired == false && isSober == true) {
-        return "You cannot drive"
-    } else if (hasDrivingLiscence == true && isTired == false && isSober == true) {
+    if (hasDrivingLiscence == false) {
+        return "You cannot drive";
+    } else if (isTired == true || isSober == false) {
+        return "You shouldn't drive";
+    } else {
         return "You can drive"
-    } else if (hasDrivingLiscence == true && isTired == true && isSober == false) {
-        return "You shouldn't drive"
-    } else if (hasDrivingLiscence == true && isTired == false && isSober == false) {
-        return "You shouldn't drive"
     }
 }
 
