c32e00810feb2bf7b038ccc777f3a4ba67646753
satyam8254 <satyamraj8254@gmail.com>
2021-11-29 19:27:10 +0530

    JavaScript Assignment 5 create object

--- assignments/create_object/create_object.js
+++ assignments/create_object/create_object.js
@@ -1,5 +1,11 @@
 function CreateObject(arr) {
     // Write your code here
+    var res = {};
+    for (var i = 0; i < arr.length; i = i + 2) {
+        res[arr[i]] = arr[i + 1]
+    }
+    return res
+
 }
 
 module.exports = CreateObject;
