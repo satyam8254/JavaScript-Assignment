9cc6e1764046058b135ccc43e989eea47fde04fd
satyam8254 <satyamraj8254@gmail.com>
2021-12-08 17:02:32 +0530

    todo api with some changes

--- assignments/todo_list/script.js
+++ assignments/todo_list/script.js
@@ -1,5 +1,8 @@
-var button = document.getElementsByTagName('button');
+var completedButton = document.getElementsByTagName('button')[0];
+var pendingButton = document.getElementsByTagName('button')[1];
 var toDolist = document.getElementsByClassName('todos')[0];
+
+
 
 
 
@@ -10,49 +13,57 @@ fetch("https://jsonplaceholder.typicode.com/todos", {
         return response.json();
     })
     .then(res => {
-        const data = res.slice(0, 10);
+        const data = res;
         console.log(data);
-        data.forEach((todo, index) => {
-            const userId = document.createElement('p');
-            const id = document.createElement('p');
-            const title = document.createElement('p');
-            const completed = document.createElement('p');
-            userId.setAttribute('key', index);
-            id.setAttribute('key', index);
-            title.setAttribute('key', index);
-            completed.setAttribute('key', index);
-            userId.innerHTML = todo.userId
-            id.innerHTML = todo.id
-            title.innerHTML = todo.title
-            completed.innerHTML = todo.completed
-            toDolist.appendChild(userId)
-            toDolist.appendChild(id)
-            toDolist.appendChild(title)
-            toDolist.appendChild(completed)
+        let completed_arr = [];
+        let pending_arr = [];
+        for (var i = 0; i < data.length; i++) {
+            if (data[i]["completed"] == true) {
+                completed_arr.push(data[i]);
+            } else {
+                pending_arr.push(data[i]);
+            }
+        }
+
+        completedButton.addEventListener('click', () => {
+            let count = 0;
+            var html = "<table border='1'>";
+            html += "<tr><th style=background-color:aqua>Sr.No.</th><th style=background-color:aqua>UserId</th><th style=background-color:aqua>id</th><th style=background-color:aqua>title</th><th style=background-color:aqua>completed</th></tr>";
+            for (var i = 0; i < completed_arr.length; i++) {
+                count++;
+                html += "<tr>";
+                html += "<td style=background-color:yellow>" + count + "</td>";
+                html += "<td style=background-color:yellow>" + completed_arr[i].userId + "</td>";
+                html += "<td style=background-color:yellow>" + completed_arr[i].id + "</td>";
+                html += "<td style=background-color:yellow>" + completed_arr[i].title + "</td>";
+                html += "<td style=background-color:green>" + completed_arr[i].completed + "</td>";
+                html += "</tr>";
+            }
+
+            html += "</table>";
+            toDolist.innerHTML = html;
         })
+
+        pendingButton.addEventListener('click', () => {
+            let count = 0;
+            var html = "<table border='1'>";
+            html += "<tr><th style=background-color:aqua>Sr.No.</th><th style=background-color:aqua>UserId</th><th style=background-color:aqua>id</th><th style=background-color:aqua>title</th><th style=background-color:aqua>completed</th></tr>";
+            for (var i = 0; i < pending_arr.length; i++) {
+                count++;
+                html += "<tr>";
+                html += "<td style=background-color:yellow>" + count + "</td>";
+                html += "<td style=background-color:yellow>" + pending_arr[i].userId + "</td>";
+                html += "<td style=background-color:yellow>" + pending_arr[i].id + "</td>";
+                html += "<td style=background-color:yellow>" + pending_arr[i].title + "</td>";
+                html += "<td style=background-color:red>" + pending_arr[i].completed + "</td>";
+                html += "</tr>";
+            }
+
+            html += "</table>";
+            toDolist.innerHTML = html;
+        })
+
     })
     .catch(err => {
         console.error(err);
     })
-
-// button.addEventListener('click', function() {
-//     toDolist.innerHTML = "";
-//     data.filter(todo => todo.completed).forEach(todo, index) => {
-//         const userId = document.createElement('p');
-//         const id = document.createElement('p');
-//         const title = document.createElement('p');
-//         const completed = document.createElement('p');
-//         userId.setAttribute('key', index);
-//         id.setAttribute('key', index);
-//         title.setAttribute('key', index);
-//         completed.setAttribute('key', index);
-//         userId.innerHTML = todo.userId
-//         id.innerHTML = todo.id
-//         title.innerHTML = todo.title
-//         completed.innerHTML = todo.completed
-//         toDolist.appendChild(userId)
-//         toDolist.appendChild(id)
-//         toDolist.appendChild(title)
-//         toDolist.appendChild(completed)
-//     }
-// })
